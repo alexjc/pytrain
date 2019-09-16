edbfd73993d9bc1eb93829f440d273141a59bcc5
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2019-09-16 23:19:37 +0200

    🛠️ Optimizing an image to match target.

---
+++ examples/train_image.py
@@ -0,0 +1,35 @@
+# PyTrain — Copyright (c) 2019, Alex J. Champandard.
+
+import torch
+import pytrain
+
+
+class MyImage(torch.nn.Module):
+    """Stores and returns a square tensor that can be optimized.
+    """
+
+    def __init__(self):
+        super(MyImage, self).__init__()
+
+        image = torch.zeros((512, 512), dtype=torch.float)
+        self.data = torch.nn.Parameter(image, requires_grad=True)
+
+    def forward(self):
+        return self.data
+
+
+class MyData:
+    """Simple iterator that returns a single randomly-chosen target image.
+    """
+
+    def __init__(self):
+        self.target = torch.empty((512, 512), dtype=torch.float).uniform_()
+
+    def __next__(self):
+        yield pytrain.Batch(target=self.target)
+
+
+@pytrain.optimize_until(iterations=1000)
+def task_optimize_image(batch: MyData, module: MyImage):
+    output = module()
+    return torch.nn.functional.mse_loss(output, batch.target)
