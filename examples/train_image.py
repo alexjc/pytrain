98d76dfa5b482fdeac49bfaa4cc2cdf87d85c9f1
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2019-10-09 18:15:07 +0200

    🛠️ Example of specifying dataset as an iterator.

--- examples/train_image.py
+++ examples/train_image.py
@@ -11,7 +11,7 @@ class MyImage(torch.nn.Module):
     def __init__(self):
         super(MyImage, self).__init__()
 
-        image = torch.zeros((512, 512), dtype=torch.float)
+        image = torch.zeros((1, 3, 256, 256), dtype=torch.float)
         self.data = torch.nn.Parameter(image, requires_grad=True)
 
     def forward(self):
@@ -23,13 +23,15 @@ class MyData:
     """
 
     def __init__(self):
-        self.target = torch.empty((512, 512), dtype=torch.float).uniform_()
+        self.target = torch.empty((1, 3, 256, 256), dtype=torch.float).uniform_()
 
     def __next__(self):
         yield pytrain.Batch(target=self.target)
 
+    def __len__(self):
+        return 1000
 
-@pytrain.terminates(iteration=1000)
+
 def task_optimize_image(data: MyData, module: MyImage):
     output = module()
     return torch.nn.functional.mse_loss(output, data.target)
