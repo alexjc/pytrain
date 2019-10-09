7838552814ceba223337a901879578c4b07c52d3
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2019-10-09 18:10:14 +0200

    🚚 Support datasets created from iterators.

--- pytrain/data.py
+++ pytrain/data.py
@@ -4,28 +4,29 @@ import torch
 
 
 class Batch:
-    def __init__(self, data, target=None):
+    def __init__(self, data=None, target=None):
         self.data = data
         self.target = target
 
     def to(self, device):
-        self.data = self.data.to(device)
+        if self.data is not None:
+            self.data = self.data.to(device)
         if self.target is not None:
             self.target = self.target.to(device)
         return self
 
 
 class Dataset:
-    def __init__(self, training, validation, testing):
+    def __init__(self, training, validation=None, testing=None):
         self.training = training
         self.validation = validation
         self.testing = testing
 
-    def length(self):
-        return self.training.shape[0]
-
     @classmethod
     def from_data(_, data, train_split=0.9):
+        if hasattr(data, "__next__"):
+            return Dataset(data)
+
         if isinstance(data, Dataset):
             return data
 
